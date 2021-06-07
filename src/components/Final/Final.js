6305a2de841197f7feac635e27c5231833de95d5
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-06-08 00:55:31 +0300

    Added animation for Final and Shers scroll

--- src/components/Final/Final.js
+++ src/components/Final/Final.js
@@ -1,23 +1,52 @@
-import React, {useContext} from 'react';
+import React, {useContext, useRef, useEffect} from 'react';
 import {Context} from '../../context';
 import { useSpring, animated } from 'react-spring';
 import Shers from '../Shers/Shers';
+import gsap from 'gsap';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
 
 import Logo from '../Logo/Logo';
 import './Final.scss';
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
 const trans1 = (x, y) => `translate3d(${x / -70}px,${y / -70}px,0)`
-const trans2 = (x, y) => `translate3d(${x / 170}px,0,0)`
 
 export default function Final() {
 
-    const {IMAGES, isMobale} = useContext(Context);
+    gsap.registerPlugin(ScrollTrigger);
 
-    const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 1000, friction: 140 } }))
+    const {IMAGES, isMobale} = useContext(Context);
+    let refFinish = useRef(null);
+
+    const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 1000, friction: 140 } }));
+
+    useEffect(() => {
+        gsap.to(refFinish, {
+            duration: 0.5,
+            opacity: 1,
+            ease: "power2.in",
+        });
+
+        gsap.to('.Shers span', {
+            duration: 0.8,
+            stagger: 0.1,
+            x: 0,
+            ease: "Back.easeOut",
+            scrollTrigger: {
+                trigger: '.Shers',
+                start: 'top 75%',
+                end: 'bottom 60%',
+                // markers: true
+            }
+        });
+    });
 
     return (
-        <div className="app-final" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+        <div
+            className="app-final"
+            onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}
+            ref={el => (refFinish = el)}
+        >
             {!isMobale && <img src={IMAGES[77].url} alt={IMAGES[77].name} className="app-final-bgimg"/>}
             {!isMobale ? (
                 <animated.img
